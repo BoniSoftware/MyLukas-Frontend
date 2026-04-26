3075eb148654cbe1abf0d5e204d4efbfec5cdfce
Carlos Andres Bonilla <venodactilo@hotmail.com>
2026-04-26 14:58:23 -0500

    Assets - Styles

--- src/pages/Registro.jsx
+++ src/pages/Registro.jsx
@@ -7,20 +7,22 @@ function Registro() {
   return (
   <>    
     <div className="containerRegistro">
+      <div className="col">
         <img src={LogoMix2} alt="Logo" className="LogoMix2" />
-        <div className="card">
-            <InputField label="Nombres :" type="text" required placeholder=" Ingrese nombre completo" />
-            <InputField label="Apellidos :" type="text" required placeholder=" Ingrese sus apellidos" />
-            <InputField label="Documento :" type="number" required placeholder=" Ingrese solo numeros" />
-            <InputField label="Correo Electronico :" type="email" required placeholder=" Ingrese email" />
-            <InputField label="Contraseña :" type="password" required placeholder=" Ingrese su contraseña" />
-            <InputField label="Confirmar Contraseña :" type="password" required placeholder=" Confirme su contraseña" />
-            <p>
-                <label htmlFor="terminos">Acepta terminos y condiciones</label>
-                <input id="terminos" type="checkbox" required />
-            </p> 
-            <Button text="Registrar Usuario" />
-        </div>
+      </div>
+      <div className="card">
+        <InputField label="Nombres :" type="text" required placeholder=" Ingrese nombre completo" />
+        <InputField label="Apellidos :" type="text" required placeholder=" Ingrese sus apellidos" />
+        <InputField label="Documento :" type="number" required placeholder=" Ingrese solo numeros" />
+        <InputField label="Correo Electronico :" type="email" required placeholder=" Ingrese email" />
+        <InputField label="Contraseña :" type="password" required placeholder=" Ingrese su contraseña" />
+        <InputField label="Confirmar Contraseña :" type="password" required placeholder=" Confirme su contraseña" />
+        <p>
+          <label htmlFor="terminos">Acepta terminos y condiciones</label>
+          <input id="terminos" type="checkbox" required />
+        </p> 
+          <Button text="Registrar Usuario" />
+      </div>
     </div>
     </>
   );
